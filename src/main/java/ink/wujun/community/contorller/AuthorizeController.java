43d2a7748d5e4c206254357a7f949d0d75a48284
a1028124300 <1028124300@qq.com>
2023-02-08 14:55:32 +0800

    add check cookies

--- src/main/java/ink/wujun/community/contorller/AuthorizeController.java
+++ src/main/java/ink/wujun/community/contorller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -39,7 +41,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String Callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -50,16 +53,17 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser != null){
-            //登录成功 写cookie 和 session
+
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
 
-            request.getSession().setAttribute("user",user);
             return "redirect:/";
         }else{
             //登录失败 重新登录
--- src/main/java/ink/wujun/community/mapper/UserMapper.java
+++ src/main/java/ink/wujun/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package ink.wujun.community.mapper;
 import ink.wujun.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author WuJun
@@ -15,5 +17,6 @@ public interface UserMapper {
     @Insert("insert user (name,account_id,token,gmt_create,gmt_modified) value (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
-
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
