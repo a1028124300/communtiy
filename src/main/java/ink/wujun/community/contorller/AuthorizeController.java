8fbac1fb072c6927feff8261224ebe7042bc59f2
a1028124300 <1028124300@qq.com>
2023-02-08 09:20:26 +0800

    extract github config from code to properties

--- src/main/java/ink/wujun/community/contorller/AuthorizeController.java
+++ src/main/java/ink/wujun/community/contorller/AuthorizeController.java
@@ -4,6 +4,7 @@ import ink.wujun.community.dto.AccessTokenDTO;
 import ink.wujun.community.dto.GithubUser;
 import ink.wujun.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,13 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
 
     @GetMapping("/callback")
     public String Callback(@RequestParam(name = "code") String code,
@@ -26,9 +34,9 @@ public class AuthorizeController {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("Iv1.0f8f1dfaffd71931");
-        accessTokenDTO.setClient_secret("5d757181000c99130697f1689f17bc86529e32cd");
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
